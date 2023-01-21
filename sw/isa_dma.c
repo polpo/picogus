8295ca209ec6d88f08ec54073fe343d37dd66d05
Ian Scott <ian@polpo.org>
2023-01-21 08:13:41 -0700

    Add missing isa dma c file

---
+++ sw/isa_dma.c
@@ -0,0 +1,20 @@
+#include "isa_dma.h"
+
+dma_inst_t DMA_init(PIO pio, irq_handler_t dma_isr) {
+    dma_inst_t dma;
+    dma.offset = pio_add_program(pio, &dma_write_program);
+    dma.sm = pio_claim_unused_sm(pio, true);
+    dma.pio = pio;
+    dma_write_program_init(pio, dma.sm, dma.offset);
+
+    if (dma_isr) {
+        uint pio_irq = (pio == pio0) ? PIO0_IRQ_0 : PIO1_IRQ_0;
+        irq_set_enabled(pio_irq, false);
+        pio_set_irq0_source_enabled(pio, pis_sm0_rx_fifo_not_empty + dma.sm, true);
+        irq_set_priority(pio_irq, PICO_HIGHEST_IRQ_PRIORITY);
+        irq_set_exclusive_handler(pio_irq, dma_isr);
+        irq_set_enabled(pio_irq, true);
+    }
+
+    return dma;
+};
