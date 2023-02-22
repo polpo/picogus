0b49e198dc4fa60be2800f8ac39820ebd87f2cbf
Ian Scott <ian@polpo.org>
2023-02-22 09:36:21 -0700

    Add missing tandyplay... whoops

---
+++ sw/tandyplay.cpp
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+#if PICO_ON_DEVICE
+
+#include "hardware/clocks.h"
+#include "hardware/structs/clocks.h"
+
+#endif
+
+#include "pico/stdlib.h"
+
+#include "pico/audio_i2s.h"
+
+#include "emu76489/emu76489.h"
+extern SNG* sn76489;
+
+#if PICO_ON_DEVICE
+#include "pico/binary_info.h"
+bi_decl(bi_3pins_with_names(PICO_AUDIO_I2S_DATA_PIN, "I2S DIN", PICO_AUDIO_I2S_CLOCK_PIN_BASE, "I2S BCK", PICO_AUDIO_I2S_CLOCK_PIN_BASE+1, "I2S LRCK"));
+#endif
+
+#define SAMPLES_PER_BUFFER 1024
+
+struct audio_buffer_pool *init_audio() {
+
+    static audio_format_t audio_format = {
+            .sample_freq = 44100,
+            .format = AUDIO_BUFFER_FORMAT_PCM_S16,
+            .channel_count = 2,
+    };
+
+    static struct audio_buffer_format producer_format = {
+            .format = &audio_format,
+            .sample_stride = 4
+    };
+
+    struct audio_buffer_pool *producer_pool = audio_new_producer_pool(&producer_format, 2,
+                                                                      SAMPLES_PER_BUFFER); // todo correct size
+    bool __unused ok;
+    const struct audio_format *output_format;
+    struct audio_i2s_config config = {
+            .data_pin = PICO_AUDIO_I2S_DATA_PIN,
+            .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
+            .dma_channel = 6,
+            .pio_sm = 1,
+    };
+
+    output_format = audio_i2s_setup(&audio_format, &config);
+    if (!output_format) {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+
+    //ok = audio_i2s_connect(producer_pool);
+    ok = audio_i2s_connect_extra(producer_pool, false, 0, 0, NULL);
+    assert(ok);
+    audio_i2s_set_enabled(true);
+    return producer_pool;
+}
+
+void play_tandy() {
+    puts("starting core 1 tandy");
+    uint32_t start, end;
+    struct audio_buffer_pool *ap = init_audio();
+    for (;;) {
+        struct audio_buffer *buffer = take_audio_buffer(ap, true);
+        int16_t *samples = (int16_t *) buffer->buffer->bytes;
+        // putchar('/');
+      
+        for (int i = 0; i < buffer->max_sample_count; ++i) {
+            samples[i << 1] = samples[(i << 1) + 1] = SNG_calc(sn76489);
+        }
+        buffer->sample_count = buffer->max_sample_count;
+
+        give_audio_buffer(ap, buffer);
+    }
+}
